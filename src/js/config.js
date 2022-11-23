9be8135905e56b2c74834a4479499a000a1e9b78
Zirodev1 <zeroygraphics@gmail.com>
2022-11-23 09:38:52 -0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fd5c3a54-b2a3-498a-b58f-d61cd0623906';
+export const MODAL_CLOSE_SEC = 2.5;
